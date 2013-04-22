31cf0c3757101c5a6415e059324ab057c1639734
Owen <owen.obyrne@iol.ie>
2013-04-22 21:27:30 +0100

    updated to perform a buy at mtgox and sell at bitcoincentral

--- src/main/java/com/owenobyrne/bitcoinarbitrage/json/TestResource.java
+++ src/main/java/com/owenobyrne/bitcoinarbitrage/json/TestResource.java
@@ -2,6 +2,7 @@ package com.owenobyrne.bitcoinarbitrage.json;
 
 import java.math.BigDecimal;
 import java.math.MathContext;
+import java.util.HashMap;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -14,6 +15,9 @@ import javax.ws.rs.core.UriInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.owenobyrne.bitcoincentral.BitcoinCentral;
+import com.owenobyrne.bitcoincentral.api.model.BitcoinCentralOrder;
+import com.owenobyrne.bitcoincentral.api.model.MarketDepth;
 import com.owenobyrne.mtgox.MtGox;
 import com.owenobyrne.mtgox.api.model.Info;
 import com.owenobyrne.mtgox.api.model.Order;
@@ -31,14 +35,20 @@ public class TestResource {
 	@Autowired
 	MtGox mtgox;
 	
+	@Autowired
+	BitcoinCentral bitcoincentral;
+	
 	@GET
-	@Produces(MediaType.TEXT_PLAIN)
-	public String respondAsReady() throws Exception {
+	@Produces(MediaType.APPLICATION_JSON)
+	public HashMap<String, String> respondAsReady() throws Exception {
 
-		Order o = mtgox.trade("bid", new BigDecimal(0.04).scaleByPowerOfTen(8).toBigInteger());
+		Order o = mtgox.trade("bid", new BigDecimal(0.01).scaleByPowerOfTen(8).toBigInteger());
+		BitcoinCentralOrder bco = bitcoincentral.trade("sell", new BigDecimal(0.01).round(new MathContext(4)));
 		
 		Info i = mtgox.getInfo();
 		Quote askq = mtgox.getQuote("ask");
+		
+		MarketDepth md = bitcoincentral.getMarketDepth();
 		
 		BigDecimal btcBalance = 
 				(i.getData().getWallets().get("BTC").getBalance().getValue_int())
@@ -46,7 +56,17 @@ public class TestResource {
 		
 		BigDecimal askPrice = (askq.getData().getAmount()).scaleByPowerOfTen(-5);
 		
-		return "Order ID: " + o.getData() + " - remaining MtGox portfolio value = " + btcBalance + " BTC @ " + askPrice + " EUR: " + btcBalance.multiply(askPrice);
+		HashMap<String, String> hm = new HashMap<String, String>();
+		hm.put("MtGox BTC", "" + btcBalance);
+		hm.put("MtGox Ask Price", "" + askPrice);
+		hm.put("MtGox EUR Value", "" + btcBalance.multiply(askPrice));
+		hm.put("BC BTC Bid", "" + md.getBids()[0].getAmount());
+		hm.put("BC Bid Price", "" + md.getBids()[0].getPrice());
+		hm.put("BC Ask UUID", bco.getUuid());
+		hm.put("MtGox Bid Order ID", o.getData());
+		
+		return hm;
+		//"Order ID: " + o.getData() + " - remaining MtGox portfolio value = " + btcBalance + " BTC @ " + askPrice + " EUR: " + btcBalance.multiply(askPrice);
 		
 	}
 }
